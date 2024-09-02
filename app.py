fde6fe31c65df9194902cc2cb0118a9097857666
DevadharshiniMani <161224154+DevadharshiniMani@users.noreply.github.com>
2024-09-02 19:50:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Book
+
+app = Flask(__name__)
+
+# Set up the database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Dev%402002@localhost/book_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database with the Flask app
+db.init_app(app)
+
+@app.route('/')
+def home():
+    books = Book.query.all()
+    return render_template("index.html", books=books)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        genre = request.form['genre']
+        price = request.form['price']
+        new_book = Book(title=title, author=author, genre=genre, price=price)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add_book.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_book(id):
+    book = Book.query.get_or_404(id)
+    if request.method == 'POST':
+        book.title = request.form['title']
+        book.author = request.form['author']
+        book.genre = request.form['genre']
+        book.price = request.form['price']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit_book.html', book=book)
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Creates all the tables defined in models.py
+    app.run(debug=True)
